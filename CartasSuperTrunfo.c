a876896f3d0df355f2eb51dd04f6b637504853c2
Jheot <josueinfoctt@gmail.com>
2025-03-15 18:56:18 +0000

    commit 4

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
 
 int main (){
-   printf("Desafio Super Trunfo");
+   printf("Desafio Super Trunfo \n");
 
-   int populacao;
-   char NomeCidade [50], Codigo [50];
+   int Populacao, PontosTuristicos;
+   char NomeCidade[50], Codigo[50];
    char Estado;
    float Area, PIB;
 
    printf("Carta 1 \n");
-   printf("Estado:");
+   printf("Digite o estado: \n");
    scanf("%s", &Estado);
+   
+   printf("Digite o código: \n");
+   scanf("%s", &Codigo[0]);
 
+   printf("Digite o nome da cidade: \n");
+   scanf("%s", &NomeCidade);
+
+   printf("Digite a população: \n");
+   scanf("%d", &Populacao);
+
+   printf("Digite a Área(km²): \n");
+   scanf("%.2f", &Area);
+
+   printf("Digite o PIB: \n");
+   scanf("%.2f", &PIB);
+
+   printf("Digite o número de pontos turísticos: \n");
+   scanf("%d", &PontosTuristicos);
+
+   return 0;a
 }
