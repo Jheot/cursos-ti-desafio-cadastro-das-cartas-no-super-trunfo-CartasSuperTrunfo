f81bfbb95c547a8102155797d94e2c41c6902fb6
Jheot <josueinfoctt@gmail.com>
2025-03-16 02:31:03 +0000

    Finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,44 +4,76 @@ int main (){
    printf("Desafio Super Trunfo \n");
 
    int Populacao, PontosTuristicos;
-   char NomeCidade[25];
-   char Codigo[25];
+   char NomeCidade[20];
+   char Codigo[3];
    char Estado;
-   float Area, PIB;
+   float Area;
+   float PIB;
 
-   printf("Carta 1 \n");
-   printf("Digite o estado: \n");
-   scanf("%s", &Estado);
+   printf("Carta 1\n");
+   printf("Digite o estado:\n");
+   scanf("%c", &Estado);
    
-   printf("Digite o código: \n");
+   printf("Digite o código:\n");
    scanf("%s", Codigo);
 
-   printf("Digite o nome da cidade: \n");
+   printf("Digite o nome da cidade:\n");
    scanf("%s", NomeCidade);
 
-   printf("Digite a população: \n");
+   printf("Digite a população:\n");
    scanf("%d", &Populacao);
 
-   printf("Digite a Área(km²): \n");
+   printf("Digite a Área(km²):\n");
    scanf("%f", &Area);
 
-   printf("Digite o PIB: \n");
+   printf("Digite o PIB:\n");
    scanf("%f", &PIB);
 
-   printf("Digite o número de pontos turísticos: \n");
+   printf("Digite o número de pontos turísticos:\n");
    scanf("%d", &PontosTuristicos);
 
-   printf("Carta 1 \n");
-   printf("Estado: %s\n", Estado);
+   printf("Carta 1\n");
+   printf("Estado: %c\n", Estado);
    printf("Código: %s\n", Codigo);
    printf("Nome da cidade: %s\n", NomeCidade);
    printf("População: %d\n", Populacao);
-   printf("Área: %d\n", Area);
+   printf("Área: %.2f (km²)\n", Area);
    printf("PIB: %.2f\n", PIB);
    printf("Número de pontos turísticos: %d\n", PontosTuristicos);
 
+   printf("\n");
+
+   printf("Carta 2\n");
+   printf("Digite o estado:\n");
+   scanf(" %c", &Estado); // Caractere de ''Espaço'' adicionado antes do "%c" para o scanf ignorar o ''printf("\n")'' acima, feito para melhor visualização do código
+   
+   printf("Digite o código:\n");
+   scanf("%s", Codigo);
+
+   printf("Digite o nome da cidade:\n");
+   scanf("%s", NomeCidade);
+
+   printf("Digite a população:\n");
+   scanf("%d", &Populacao);
+
+   printf("Digite a Área(km²):\n");
+   scanf("%f", &Area);
+
+   printf("Digite o PIB:\n");
+   scanf("%f", &PIB);
+
+   printf("Digite o número de pontos turísticos:\n");
+   scanf("%d", &PontosTuristicos);
+
+   printf("Carta 2\n");
+   printf("Estado: %c\n", Estado);
+   printf("Código: %s\n", Codigo);
+   printf("Nome da cidade: %s\n", NomeCidade);
+   printf("População: %d\n", Populacao);
+   printf("Área: %.2f (km²)\n", Area); // por conta das restrições atribuidas a esta atividade, o resultado de printf("Área: %.2f...") irá exibir, EX: Área 123456.00 (km²) ao invés de 1234.56 (km²)  
+   printf("PIB: %.2f\n", PIB);
+   printf("Número de pontos turísticos: %d\n", PontosTuristicos);
+   
    return 0;
-
-
 
 }
