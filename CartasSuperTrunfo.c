4516b1edcfff441f272f5d84f2276a316a521b5a
Jheot <josueinfoctt@gmail.com>
2025-03-16 01:00:03 +0000

    commit 6

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,8 @@ int main (){
    printf("Desafio Super Trunfo \n");
 
    int Populacao, PontosTuristicos;
-   char NomeCidade[50], Codigo[50];
+   char NomeCidade[25];
+   char Codigo[25];
    char Estado;
    float Area, PIB;
 
@@ -13,23 +14,34 @@ int main (){
    scanf("%s", &Estado);
    
    printf("Digite o código: \n");
-   scanf("%s", &Codigo[0]);
+   scanf("%s", Codigo);
 
    printf("Digite o nome da cidade: \n");
-   scanf("%s", &NomeCidade);
+   scanf("%s", NomeCidade);
 
    printf("Digite a população: \n");
    scanf("%d", &Populacao);
 
    printf("Digite a Área(km²): \n");
-   scanf("%.2f", &Area);
+   scanf("%f", &Area);
 
    printf("Digite o PIB: \n");
-   scanf("%.2f", &PIB);
+   scanf("%f", &PIB);
 
    printf("Digite o número de pontos turísticos: \n");
    scanf("%d", &PontosTuristicos);
 
-   printf("Estado: %d - Código: %s \n", Estado, Codigo);
-   printf ("Nome da Cidade: %s - População: %d \n", NomeCidade, Populacao);
+   printf("Carta 1 \n");
+   printf("Estado: %s\n", Estado);
+   printf("Código: %s\n", Codigo);
+   printf("Nome da cidade: %s\n", NomeCidade);
+   printf("População: %d\n", Populacao);
+   printf("Área: %d\n", Area);
+   printf("PIB: %.2f\n", PIB);
+   printf("Número de pontos turísticos: %d\n", PontosTuristicos);
+
+   return 0;
+
+
+
 }
