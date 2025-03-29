2b8c0dd18b08320b3b4172b06f2cbfc67b8784c1
Jheot <josueinfoctt@gmail.com>
2025-03-29 20:37:27 +0000

    Desafio 2 finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,77 +3,109 @@
 int main (){
    printf("Desafio Super Trunfo \n");
 
-   int PontosTuristicos, Populacao;
-   char NomeCidade[30];
-   char Codigo[3];
-   char Estado[25];
-   float Area;
-   float PIB;
+   int PontosTuristicos;
+   char NomeCidade1[30], NomeCidade2[30];
+   char Codigo1[3], Codigo2[3];
+   char Estado1[24], Estado2[25];
+   float Area1, Area2;
+   float PIB1, PIB2;
+   float Populacao1, Populacao2;
+   float Densidade1, Densidade2;
+   float PerCapita1, PerCapita2;
 
+  // CARTA 1
 
    printf("Carta1\n");
    printf("Digite o estado:\n");
-   scanf("%s", Estado);
+   scanf("%23s", Estado1);
    
    printf("Digite o código:\n");
-   scanf("%s", Codigo);
+   scanf("%2s", Codigo1);
 
    printf("Digite o nome da cidade:\n");
-   scanf("%s", NomeCidade);
+   scanf("%29s", NomeCidade1);
 
    printf("Digite a população:\n");
-   scanf("%d", &Populacao);
+   scanf("%f", &Populacao1);
 
    printf("Digite a Área(km²):\n");
-   scanf("%f", &Area);
+   scanf("%f", &Area1);
 
    printf("Digite o PIB:\n");
-   scanf("%f", &PIB);
+   scanf("%f", &PIB1);
 
    printf("Digite o número de pontos turísticos:\n");
    scanf("%d", &PontosTuristicos);
 
    printf("Carta1\n");
-   printf("Estado: %s\n", Estado);
-   printf("Código: %s\n", Codigo);
-   printf("Nome da cidade: %s\n", NomeCidade);
-   printf("População: %d\n", Populacao);
-   printf("Área: %.2f (km²)\n", Area);
-   printf("PIB: %.2f\n", PIB);
+   printf("Estado: %s\n", Estado1);
+   printf("Código: %s\n", Codigo1);
+   printf("Nome da cidade: %s\n", NomeCidade1);
+   printf("População: %f\n", Populacao1);
+   printf("Área: %.2f (km²)\n", Area1);
+   printf("PIB: %.2f\n", PIB1);
    printf("Número de pontos turísticos: %d\n", PontosTuristicos);
 
    printf("\n");
 
+   //CARTA 2
+
    printf("Carta2\n");
    printf("Digite o estado:\n");
-   scanf(" %s", Estado); 
+   scanf(" %24s", Estado2); 
    
    printf("Digite o código:\n");
-   scanf("%s", Codigo);
+   scanf("%2s", Codigo2);
 
    printf("Digite o nome da cidade:\n");
-   scanf("%s", NomeCidade);
+   scanf("%29s", NomeCidade2);
 
    printf("Digite a população:\n");
-   scanf("%d", &Populacao);
+   scanf("%f", &Populacao2);
 
    printf("Digite a Área(km²):\n");
-   scanf("%f", &Area);
+   scanf("%f", &Area2);
 
    printf("Digite o PIB:\n");
-   scanf("%f", &PIB);
+   scanf("%f", &PIB2);
 
    printf("Digite o número de pontos turísticos:\n");
    scanf("%d", &PontosTuristicos);
 
    printf("Carta2\n");
-   printf("Estado: %s\n", Estado);
-   printf("Código: %s\n", Codigo);
-   printf("Nome da cidade: %s\n", NomeCidade);
-   printf("População: %d\n", Populacao);
-   printf("Área: %.2f (km²)\n", Area); // por conta das restrições atribuidas a esta atividade, o resultado de printf("Área: %.2f...") irá exibir, EX: Área 123456.00 (km²) ao invés de 1234.56 (km²)  
-   printf("PIB: %.2f\n", PIB);
+   printf("Estado: %s\n", Estado2);
+   printf("Código: %s\n", Codigo2);
+   printf("Nome da cidade: %s\n", NomeCidade2);
+   printf("População: %f\n", Populacao2);
+   printf("Área: %.2f (km²)\n", Area2); // por conta das restrições atribuidas a esta atividade, o resultado de printf("Área: %.2f...") irá exibir, EX: Área 123456.00 (km²) ao invés de 1234.56 (km²)  
+   printf("PIB: %.2f\n", PIB2);
    printf("Número de pontos turísticos: %d\n", PontosTuristicos);
  
- return 0;   
+   Densidade1 = (float) (Populacao1 / Area1);
+   printf ("A densidade populacional da carta 1 é: %.2f km²\n", Densidade1);
+
+   Densidade2 = (float) (Populacao2 / Area2);
+   printf ("A densidade Populacao da carta 2 é: %.2f km²\n", Densidade2);
+
+   if (Densidade1 < Densidade2){
+    printf("A carta 1 venceu");
+  }else{
+    printf("A carta 2 venceu");
+  }
+
+  printf("\n");
+
+  PerCapita1 = (float) (PIB1 / Populacao1);
+  printf ("A PIB per capita da carta 1 é: %.2f\n", PerCapita1);
+
+  PerCapita2 = (float) (PIB2 / Populacao2);
+  printf ("O PIB per capita da carta 2 é: %.2f\n", PerCapita2);
+ 
+  if (PerCapita1 > PerCapita2){
+    printf ("A carta 1 venceu");
+  }else{
+    printf ("A carta 2 venceu");
+  }
+
+  return 0;
 }
---
+++ TrunfoTema2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main ({  // utilizei um repósitório diferente para não interferir no primeiro trabalho
+    
+
+
+    
+})
